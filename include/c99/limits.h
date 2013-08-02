e5df017df3760f4b1d1cc78552bfbee4aca02094
Jean-David Gadina <macmade@xs-labs.com>
2013-08-02 20:28:27 +0200

    C99 - limits.h

--- include/c99/limits.h
+++ include/c99/limits.h
@@ -68,37 +68,11 @@
 extern "C" {
 #endif
 
-#define SCHAR_MAX           0x7F
-#define SHRT_MAX            0x7FFF
-#define INT_MAX             0x7FFFFFFF
-
-#ifdef __LP64__
-    
-    #define LONG_MAX        0x7FFFFFFFFFFFFFFFL
-    
-#else
-    
-    #define LONG_MAX        0x7FFFFFFF
-    
-#endif
-
-#define SCHAR_MIN           ( -SCHAR_MAX - 1  )
-#define SHRT_MIN            ( -SHRT_MAX  - 1  )
-#define INT_MIN             ( -INT_MAX   - 1  )
-#define LONG_MIN            ( -LONG_MAX  - 1L )
+#define CHAR_BIT            0x08
 
 #define UCHAR_MAX           ( SCHAR_MAX * 2   + 1   )
-#define USHRT_MAX           ( SHRT_MAX  * 2   + 1   )
-#define UINT_MAX            ( INT_MAX   * 2U  + 1U  )
-#define ULONG_MAX           ( LONG_MAX  * 2UL + 1UL )
-
-#if __STDC_VERSION__ >= 199901
-
-    #define LLONG_MAX       0x7FFFFFFFFFFFFFFFL
-    #define LLONG_MIN       ( -LLONG_MAX - 1LL )
-    #define ULLONG_MAX      ( LLONG_MAX * 2ULL + 1ULL )
-
-#endif
+#define SCHAR_MIN           ( -SCHAR_MAX - 1  )
+#define SCHAR_MAX           0x7F
 
 #ifdef __CHAR_UNSIGNED__
     
@@ -112,8 +86,36 @@ extern "C" {
     
 #endif
 
-#define CHAR_BIT            0x08
 #define MB_LEN_MAX          0x01
+
+#define USHRT_MAX           ( SHRT_MAX  * 2   + 1   )
+#define SHRT_MIN            ( -SHRT_MAX  - 1  )
+#define SHRT_MAX            0x7FFF
+
+#define UINT_MAX            ( INT_MAX   * 2U  + 1U  )
+#define INT_MIN             ( -INT_MAX   - 1  )
+#define INT_MAX             0x7FFFFFFF
+
+#define ULONG_MAX           ( LONG_MAX  * 2UL + 1UL )
+#define LONG_MIN            ( -LONG_MAX  - 1L )
+
+#ifdef __LP64__
+    
+    #define LONG_MAX        0x7FFFFFFFFFFFFFFFL
+    
+#else
+    
+    #define LONG_MAX        0x7FFFFFFF
+    
+#endif
+
+#if __STDC_VERSION__ >= 199901
+
+    #define LLONG_MAX       0x7FFFFFFFFFFFFFFFL
+    #define LLONG_MIN       ( -LLONG_MAX - 1LL )
+    #define ULLONG_MAX      ( LLONG_MAX * 2ULL + 1ULL )
+
+#endif
 
 #ifdef __cplusplus
 }
